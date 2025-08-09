6b47cb36323ed3e7d5467e85ec84fab6d7d42425
Aleksandar <musicianstoragebox@gmail.com>
2025-08-09 20:27:31 +0200

    routes protection

--- backend/routes/publicAPIRoutes.js
+++ backend/routes/publicAPIRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { getAQ , getForecastCNT } = require('../controllers/publicAPIController');
-
+const { requireAuth, requireRole } = require('../services/middleware')
 const router = express.Router();
+
+router.use(requireAuth, requireRole('administrator'));
 
 router.get('/getAirQuality', getAQ);
 router.get('/getForecast', getForecastCNT);
